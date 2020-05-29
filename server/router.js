8c1fea88f001579d1c038a88427b225d9ed3f0b6
Joel Santana <joelsantzdev@gmail.com>
2020-05-29 12:18:22 -0500

    Revert "Feature/express structure"

--- server/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send('El servidor esta arriba y corriendo...');
-});
-
-module.exports = router;
