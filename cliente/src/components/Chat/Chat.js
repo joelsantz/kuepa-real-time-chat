db467547e711dde8fe01734c2dc2626d9779a1b0
joelsantz <joelsantzdev@gmail.com>
2020-05-29 20:12:34 -0500

    add useEffect hook in chat.js

--- cliente/src/components/Chat/Chat.js
+++ cliente/src/components/Chat/Chat.js
@@ -7,12 +7,31 @@ import './Chat.css';
 let socket;
 
 const Chat = ({ location }) => {
-    
+
     const [name, setName] = useState("");
     const [room, setRoom] = useState("");
     const [users, setUsers] = useState("");
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
+    
+    useEffect(() => {
+        socket.on('message', message => {
+          setMessages(messages => [ ...messages, message ]);
+        });
+        
+        socket.on("roomData", ({ users }) => {
+          setUsers(users);
+        });
+    }, []);
+    
+      const sendMessage = (event) => {
+        event.preventDefault();
+    
+        if(message) {
+          socket.emit('sendMessage', message, () => setMessage(''));
+        }
+      }
+
     return (
         <h1>Chat</h1>
     )
