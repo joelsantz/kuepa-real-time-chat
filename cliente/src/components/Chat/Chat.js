1f046bb4e188ac75270b5b9987eb0adcb72a5faf
joelsantz <joelsantzdev@gmail.com>
2020-05-29 20:21:18 -0500

    add socket.io confg

--- cliente/src/components/Chat/Chat.js
+++ cliente/src/components/Chat/Chat.js
@@ -16,6 +16,21 @@ const Chat = ({ location }) => {
     const ENDPOINT = 'localhost:5000';
     
     useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+    
+        socket = io(ENDPOINT);
+    
+        setRoom(room);
+        setName(name)
+    
+        socket.emit('join', { name, room }, (error) => {
+          if(error) {
+            alert(error);
+          }
+        });
+      }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
         socket.on('message', message => {
           setMessages(messages => [ ...messages, message ]);
         });
@@ -34,8 +49,15 @@ const Chat = ({ location }) => {
       }
 
     return (
-        <h1>Chat</h1>
-    )
+        <div className="outerContainer">
+        <div className="container">
+            <InfoBar room={room} />
+            <Messages messages={messages} name={name} />
+            <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+        </div>
+        <TextContainer users={users}/>
+      </div>
+    );
 }
 
 export default Chat;
