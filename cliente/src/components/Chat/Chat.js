0228fa68f2894c5699e25facb6d89b5c7721c971
joelsantz <joelsantzdev@gmail.com>
2020-05-29 20:09:39 -0500

    add useState hook

--- cliente/src/components/Chat/Chat.js
+++ cliente/src/components/Chat/Chat.js
@@ -2,7 +2,17 @@ import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from "socket.io-client";
 
-const Chat = () => {
+import './Chat.css';
+
+let socket;
+
+const Chat = ({ location }) => {
+    
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
+    const [users, setUsers] = useState("");
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
     return (
         <h1>Chat</h1>
     )
