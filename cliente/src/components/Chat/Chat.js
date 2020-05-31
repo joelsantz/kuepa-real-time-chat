70753a0d8c667cda6f89100e72fffb5263e4ae89
joelsantz <joelsantzdev@gmail.com>
2020-05-30 23:54:32 -0500

    conf files

--- cliente/src/components/Chat/Chat.js
+++ cliente/src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState("");
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://kuepa-chat-joelsantana.herokuapp.com/';
     
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
